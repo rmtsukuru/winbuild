f2aaa5abd89780125fec360418d63600157d6f77
Samantha Randolph <samcollegeemail@gmail.com>
2026-05-30 11:19:39 -0700

    Create initial winbuild.bat

---
+++ winbuild.bat
@@ -0,0 +1,6 @@
+:: Let's build a windows dev environment~
+
+:: Install Git for Windows (including Git Bash)
+winget install --id Git.Git -e --source winget
+
+:: Add comments for installing mingw-w64, downloading dotfiles, etc.

11c0bd26a1bd204a61e960647adb69bca0491b5d
Samantha Randolph <samcollegeemail@gmail.com>
2026-06-06 13:14:03 -0700

    Updated batch script files to use .cmd instead of .bat and REM instead of :: for comments.
    Apparently the other methods can cause errors in some situations. See here for more details: https://steve-jansen.github.io/guides/windows-batch-scripting/part-1-getting-started.html

--- fetchbuild.bat
+++
@@ -1,2 +0,0 @@
-powershell wget https://raw.githubusercontent.com/rmtsukuru/winbuild/refs/heads/main/winbuild.bat -OutFile winbuild.bat
-
--- winbuild.cmd
+++ winbuild.cmd
@@ -1,11 +1,11 @@
-:: Let's build a windows dev environment~
+REM Let's build a windows dev environment~
 
-:: Install Git for Windows (including Git Bash)
+REM Install Git for Windows (including Git Bash)
 winget install --id Git.Git -e --source winget
 
-:: TODO Add remaining commands for installing mingw-w64, downloading dotfiles, etc.
+REM TODO Add remaining commands for installing mingw-w64, downloading dotfiles, etc.
 powershell wget https://github.com/brechtsanders/winlibs_mingw/releases/download/16.1.0posix-14.0.0-msvcrt-r2/winlibs-x86_64-posix-seh-gcc-16.1.0-mingw-w64msvcrt-14.0.0-r2.zip -Outfile winlibs-mingw-w64.zip
 
 
-:: Don't forget to clean up random remaining files at the end
+REM Don't forget to clean up random remaining files at the end
 

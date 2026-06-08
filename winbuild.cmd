9f3b4d3d133a34aab345a2a6d9c76d5ad4c4b4ba
Samantha Randolph <samcollegeemail@gmail.com>
2026-06-07 21:46:45 -0700

    Add command to run shell script to main batch script.

--- winbuild.cmd
+++ winbuild.cmd
@@ -6,6 +6,9 @@ winget install --id Git.Git -e --source winget
 REM TODO Add remaining commands for installing mingw-w64, downloading dotfiles, etc.
 powershell wget https://github.com/brechtsanders/winlibs_mingw/releases/download/16.1.0posix-14.0.0-msvcrt-r2/winlibs-x86_64-posix-seh-gcc-16.1.0-mingw-w64msvcrt-14.0.0-r2.zip -Outfile winlibs-mingw-w64.zip
 
+REM Run bash shell script for remaining installation tasks (if possible)
+REM TODO Double-check that we have the right path to sh or that we can add its directory to the PATH
+sh winbuild.sh
 
 REM Don't forget to clean up random remaining files at the end
 

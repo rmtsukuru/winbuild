ef53c371c90c937f33003aeca45f416e27e0b075
Samantha Randolph <samcollegeemail@gmail.com>
2026-06-07 21:48:49 -0700

    Added winbuild.sh.

---
+++ winbuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# TODO Add commands for setting everything up to pick up where winbuild.cmd left off
+
+# This should already have winlibs installed at this point? We'll see
+
+# This includes downloading and copying over dotfiles
+# TODO add dotfiles (esp. bashrc, bash_aliases, and vimrc) to this repo for easy downloading

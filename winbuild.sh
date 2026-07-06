6774b3bd09eb14e9bb2b1d4cc6785c46182d6906
Samantha Randolph <samcollegeemail@gmail.com>
2026-07-06 16:54:41 -0700

    Add something useful to winbuild.sh

--- winbuild.sh
+++ winbuild.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+echo "Running Bash installation script... (hold tight, almost done with everything!)"
 
 # TODO Add commands for setting everything up to pick up where winbuild.cmd left off
 
 # This should already have winlibs installed at this point? We'll see
+# Correction: winlibs should already be updated, now needs to be moved into correct directories and set up export
+# so the PATH is set correctly (see section on dotfiles below for adding that in bashrc/bash_profile)
 
 # This includes downloading and copying over dotfiles
 # TODO add dotfiles (esp. bashrc, bash_aliases, and vimrc) to this repo for easy downloading
